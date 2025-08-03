2d23fcdad8d3ef0a7a86443ae744594c57ccecd9
haloguoguo <90536852+haloguoguo@users.noreply.github.com>
2025-08-03 22:59:45 +0800

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -148,7 +148,7 @@ const Sidebar = ({ onToggle, activePath = '/' }: SidebarProps) => {
     {
       icon: Clapperboard,
       label: '英剧',
-      href: '/douban?type=custom2&=英剧',
+      href: '/douban?type=custom2&tag=英剧',
     },
     {
       icon: Drum,
