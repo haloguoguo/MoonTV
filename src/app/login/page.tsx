497d26c1a714b93eedcdcbf3016124874e0c504a
haloguoguo <90536852+haloguoguo@users.noreply.github.com>
2025-08-04 19:27:29 +0800

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -159,7 +159,8 @@ function LoginPageClient() {
       </div>
       <div className='relative z-10 w-full max-w-md rounded-3xl bg-gradient-to-b from-white/90 via-white/70 to-white/40 dark:from-zinc-900/90 dark:via-zinc-900/70 dark:to-zinc-900/40 backdrop-blur-xl shadow-2xl p-10 dark:border dark:border-zinc-800'>
         <h1 className='text-green-600 tracking-tight text-center text-3xl font-extrabold mb-8 bg-clip-text drop-shadow-sm'>
-          {siteName}每天都是新的开始
+          {siteName}
+          每天都是新的开始
         </h1>
         <form onSubmit={handleSubmit} className='space-y-8'>
           {shouldAskUsername && (
